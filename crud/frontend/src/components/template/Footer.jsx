dc2d18b331e87139a09462d2946a990249925d84
João V G Carrijo <jvgcarrijo@outlook.com>
2024-07-16 11:17:39 -0300

    Add files via upload

---
+++ crud/frontend/src/components/template/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Footer.css';
+
+const Footer = props => {
+    return (
+        <footer className='footer'>
+            <span>
+                Desenvolvido com <i className='fa fa-heart text-danger'></i> por <strong>João Víctor Guedes Carrijo!</strong>
+            </span>
+        </footer>
+    )
+}
+
+export default Footer;
